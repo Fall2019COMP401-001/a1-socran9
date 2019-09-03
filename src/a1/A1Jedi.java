1fb4bc04275f556d6cc922392a9762c3c213b600
cranford0209 <cranford0209@silass-air.lan>
2019-09-03 13:12:49 -0400

    Finished the code

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,5 +1,6 @@
 package a1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class A1Jedi {
@@ -9,6 +10,45 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int countofitems = scan.nextInt();
+		ArrayList<String> itemList = new ArrayList<String>();
+		Double [] itemPrices = new Double [countofitems];
 		
-	}
+		// Arrays to track the name and price of each item matching the first item to the first price
+		for (int i = 0; i < countofitems; i++) {
+			itemList.add(scan.next());
+			itemPrices[i] = scan.nextDouble();
+		}
+		
+		int customerCount = scan.nextInt();
+		String[] customerNames = new String[customerCount];
+		int[] totalQuantityBought = new int[countofitems];
+		int[] differentCustomersBought = new int[countofitems];
+		
+		for(int i = 0; i < customerCount; i++) {
+			boolean[] foodAlreadyBought = new boolean[countofitems];
+			// Initialize boolean array
+			for(int y = 0; y < countofitems; y++) { foodAlreadyBought[y] = false;}
+			customerNames[i] = scan.next() + " " + scan.next();
+			int customerItemCount = scan.nextInt();
+			for(int y = 0; y < customerItemCount; y++) {
+				int specificItemCount = scan.nextInt();
+				int indexPos = itemList.indexOf(scan.next());
+				if (foodAlreadyBought[y] == false) {
+					foodAlreadyBought[y] = true;
+					differentCustomersBought[indexPos] += 1;
+				}
+				totalQuantityBought[indexPos] += specificItemCount;
+			}
+		}
+		
+		for(int i = 0; i < countofitems; i++) {
+			if(differentCustomersBought[i] == 0) {
+				System.out.println("No customers bought " + itemList.get(i));
+			} else {
+				System.out.println(differentCustomersBought[i] + " customers bought " + totalQuantityBought[i] + " " + itemList.get(i));
+			}
+		}
+		
+	}	
 }
